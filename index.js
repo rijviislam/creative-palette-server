d020d6e47072551a1f038313ef6035fffa67b4a6
Rijvi Islam <rijviislam54@gmail.com>
2024-04-27 00:09:25 +0600

    feat: added post and get method for craft section

--- index.js
+++ index.js
@@ -25,6 +25,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const craftCollection = client.db('craftItemDB').collection('craftItem')
+
+    app.get('/craftitem', async(req, res) => {
+        const cursor = craftCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.post('/craftitem', async(req, res) => {
+        const newCraftItem = req.body;
+        console.log(newCraftItem);
+        const result = await craftCollection.insertOne(newCraftItem);
+        res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
