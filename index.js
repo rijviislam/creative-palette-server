ecd2a4556c2941ef36eb1667dd8941235399276b
Rijvi Islam <rijviislam54@gmail.com>
2024-04-29 19:07:40 +0600

    revert : reverts a previous commit

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ async function run() {
     await client.connect();
 
     const craftCollection = client.db("craftItemDB").collection("craftItem");
+    const canvasCollection = client
+      .db("craftItemDB")
+      .collection("popularCategories");
     const artcraftCollection = client
       .db("craftItemDB")
       .collection("ArtAndCraftCategories");
@@ -40,7 +43,11 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
-
+    app.get("/canvas", async (req, res) => {
+      const cursor = canvasCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     app.get("/craftitem/:id", async (req, res) => {
       const id = req.params.id;
@@ -49,7 +56,13 @@ async function run() {
       res.send(result);
     });
 
+    // app.get('/craftitem/:subcategory_Name' , async(req, res) => {
+    //   const subCategory = req.params.subcategory_Name;
+    //   const query = {subcategory_Name : new ObjectId(subcategory_Name)};
+    //   const result = await craftCollection.find(query)
+    // })
 
+   
     // app.get(`/craftitem/:subcategory_Name`, async(req, res) => {
     //     console.log(req.params.subcategory_Name);
     //     const query = { subcategory_Name: "Craft" };
@@ -61,16 +74,14 @@ async function run() {
     //     console.log(result);
     // })
 
+    // app.get("/craftitem/:subcategory_Name", async (req, res) => {
+    //   const subcategory_Name = req.params.subcategory_Name;
+    //   const query = { subcategory_Name: subcategory_Name };
+    //   const result = await craftCollection.find(query).toArray();
+    //   res.send(result);
+    //   // console.log(result)
 
-
-    app.get("/craftitem/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await craftCollection.findOne(query);
-      res.send(result);
-    });
-
-
+    
     app.get("/craftitem", async (req, res) => {
       const email = req.query.email;
       if (email) {
@@ -88,27 +99,31 @@ async function run() {
       res.send(result);
     });
 
-    app.put('/craftitemupdate/:id', async(req, res) => {
+    app.put("/craftitemupdate/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id : new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
       const updatedProduct = req.body;
-      const  updatedProductItem = {
+      const updatedProductItem = {
         $set: {
           image: updatedProduct.image,
           name: updatedProduct.name,
           price: updatedProduct.price,
           processing_time: updatedProduct.processing_time,
-          rating:updatedProduct.rating,
+          rating: updatedProduct.rating,
           shortdescription: updatedProduct.shortdescription,
           stockStatus: updatedProduct.stockStatus,
           subcategory_Name: updatedProduct.subcategory_Name,
           customization: updatedProduct.customization,
-        }
-      }
-      const result = await craftCollection.updateOne(filter, updatedProductItem, options);
+        },
+      };
+      const result = await craftCollection.updateOne(
+        filter,
+        updatedProductItem,
+        options
+      );
       res.send(result);
-    })
+    });
 
     app.delete("/craftitem/:id", async (req, res) => {
       const id = req.params.id;
